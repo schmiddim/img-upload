2927e0d2987ae70c16391aba702bcf52b2773445
ms <spam@example.com>
2011-05-17 09:58:58 +0200

    login

---
+++ Login.php
@@ -0,0 +1,38 @@
+<?php
+class Login{
+	private $login=false;
+	private $user;
+	private $password;
+	public function __construct(){
+		session_start();
+		$this->user="michael";
+		$this->password=md5("nutellamitwurst");
+
+
+	}//__construct
+
+	public function validateLogin($user, $password){
+		//session started?
+		if(isset($_SESSION['user']) && isset($_SESSION['password'])){				
+			$user=$_SESSION['user'] ;
+			$password=$_SESSION['password'] ;
+		}			
+		if ($user=$this->user && $this->password == md5($password)){
+				
+			$_SESSION['user'] = $user;
+			$_SESSION['password'] = $password;
+			$this->login=true;				
+		}
+	}//validateLogin
+
+	public function logout($logoutVar){
+		if (isset($logoutVar) && $logoutVar){
+			session_destroy();
+			$this->login=false;
+		}
+	}
+	public function loggedIn(){
+
+		return $this->login;
+	}
+}
